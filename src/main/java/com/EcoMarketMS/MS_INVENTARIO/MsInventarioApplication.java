5d87d4665594b81a310b392040ccf0bdf8f7040b
Felipe Inzunza <feli.inzunza@duocuc.cl>
2025-05-11 18:07:55 -0400

    cambiouno

---
+++ src/main/java/com/EcoMarketMS/MS_INVENTARIO/MsInventarioApplication.java
@@ -0,0 +1,16 @@
+package com.EcoMarketMS.MS_INVENTARIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInventarioApplication.class, args);
+	}
+
+}
+
+
+
